1307491606ce53b9a48a1f19c8d72693a838daad
behicehafsa <behiceozmenkolay@gmail.com>
2021-10-14 16:06:44 +0300

    added app.py file

--- python/hands-on/flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods_2/app.py
+++ python/hands-on/flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods_2/app.py
@@ -1,23 +1,24 @@
-# Create an object named app
-from flask import Flask, request, render_template
+# Import Flask modules
+from flask import Flask, render_template, request
 
+
+# Create an object named app
 app = Flask(__name__)
 
 # create a function named "lcm" which calculates a least common multiple values of two numbers. 
-def lcm(num1, num2):
-    common_multiplication = []
-    for i in range(max(num1,num2), num1*num2+1):
-        if i%num1==0 and i%num2==0:
-            common_multiplication.append(i)
-    return min(common_multiplication)
-
+def lcm(num1,num2):
+    common_multiplications = [] 
+    for i in range(max(num1, num2),num1*num2+1): 
+        if i%num1==0 and i%num2==0: 
+           common_multiplications.append(i) 
+    return min(common_multiplications)
 
 # Create a function named `index` which uses template file named `index.html` 
 # send two numbers as template variable to the app.py and assign route of no path ('/') 
-
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template('index.html', methods = ["GET"])
+
 
 # calculate sum of them using "lcm" function, then sent the result to the 
 # "result.hmtl" file and assign route of path ('/calc'). 
@@ -25,14 +26,13 @@ def index():
 @app.route("/calc", methods = ["GET", "POST"])
 def calculate():
     if request.method == "POST":
-        num1 = request.form.get("number1")
-        num2 = request.form.get("number2")
-        return render_template("result.html", var1=num1, var2=num2, result=lcm(int(num1),int(num2)), developer_name="Serdar")
+        mynumber1 = request.form.get("number1")
+        mynumber2 = request.form.get("number2")
+        return render_template('result.html', var1=mynumber1, var2=mynumber2, result=lcm(int(mynumber1), int(mynumber2)), developer_name="behice")
     else:
-        return render_template("result.html", developer_name="Serdar")
-
-
+        return render_template('result.html', developer_name="behice")
 # Add a statement to run the Flask application which can be debugged.
-if __name__== "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
-    #app.run(host='0.0.0.0', port=80)
+
+
